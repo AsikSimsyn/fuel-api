d4d2da98e1cd4b1fa5345bc257798afac03a226d
AsikSimsyn <asik.kafoor@simsyn.com>
2023-06-18 10:09:36 +0530

    pool added

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
-const { Client } = require('pg')
-const client = new Client(process.env.DATABASE_URL)
-client.connect()
+const { Client, Pool } = require('pg')
+// const client = new Client(process.env.DATABASE_URL)
+// client.connect()
+
+const pool = new Pool({
+    user: 'fuel_db_user',
+    host: 'dpg-chfrogu7avjbbjpunvh0-a.singapore-postgres.render.com',
+    database: 'fuel_db',
+    password: 'u7OIkPidCZHq980ONCYXjh59dSZZDr1X',
+    port: 5432,
+    ssl: true
+  })
 
 app.get("/api/v1/add", async (req, res) => {
     const { usage } = req.query;
@@ -14,7 +23,7 @@ app.get("/api/v1/add", async (req, res) => {
     let date = d.toISOString().slice(0,10);
     console.log(date)
     try {
-        const results = await client.query("insert into test(fuel_usage, date) values('" + usage + "','" + date + "');");
+        const results = await pool.query("insert into test(fuel_usage, date) values('" + usage + "','" + date + "');");
         res.json("record added successfully")
 
     } catch (err) {
@@ -26,7 +35,7 @@ app.get("/api/v1/add", async (req, res) => {
 
 app.get("/api/v1/get", async (req, res) => {
     try {
-        const results = await client.query("select * from test");
+        const results = await pool.query("select * from test");
         res.send(results.rows)
     } catch (err) {
         // console.error("error executing query:", err);
@@ -37,7 +46,7 @@ app.get("/api/v1/get", async (req, res) => {
 
 app.get("/api/fuel/daily", async (req, res) => {
     try {
-        const results = await client.query("SELECT TO_CHAR(date, 'Day') AS day_name, SUM(fuel_usage) AS fuel_usage FROM tbl_fuel_usage WHERE DATE(date) = CURRENT_DATE AND EXTRACT(month FROM date) = EXTRACT(month FROM CURRENT_DATE) GROUP BY day_name;");
+        const results = await pool.query("SELECT TO_CHAR(date, 'Day') AS day_name, SUM(fuel_usage) AS fuel_usage FROM tbl_fuel_usage WHERE DATE(date) = CURRENT_DATE AND EXTRACT(month FROM date) = EXTRACT(month FROM CURRENT_DATE) GROUP BY day_name;");
         res.send(results.rows)
     } catch (err) {
         // console.error("error executing query:", err);
@@ -48,7 +57,7 @@ app.get("/api/fuel/daily", async (req, res) => {
 
 app.get("/api/fuel/weekly", async (req, res) => {
     try {
-        const results = await client.query("SELECT DATE_TRUNC('week', date) AS week_start_date,EXTRACT(week FROM date) - EXTRACT(week FROM DATE_TRUNC('month', date)) + 1 AS week_in_month,SUM(fuel_usage) AS total_fuel_usage FROM tbl_fuel_usage WHERE EXTRACT(month FROM date) = EXTRACT(month FROM CURRENT_DATE) GROUP BY week_start_date, week_in_month ORDER BY week_start_date;");
+        const results = await pool.query("SELECT DATE_TRUNC('week', date) AS week_start_date,EXTRACT(week FROM date) - EXTRACT(week FROM DATE_TRUNC('month', date)) + 1 AS week_in_month,SUM(fuel_usage) AS total_fuel_usage FROM tbl_fuel_usage WHERE EXTRACT(month FROM date) = EXTRACT(month FROM CURRENT_DATE) GROUP BY week_start_date, week_in_month ORDER BY week_start_date;");
         res.send(results.rows)
     } catch (err) {
         // console.error("error executing query:", err);
@@ -59,7 +68,7 @@ app.get("/api/fuel/weekly", async (req, res) => {
 
 app.get("/api/fuel/monthly", async (req, res) => {
     try {
-        const results = await client.query("SELECT TO_CHAR(date, 'Month') AS month_name, SUM(fuel_usage) AS total_fuel_usage FROM tbl_fuel_usage GROUP BY month_name ORDER BY MIN(date);");
+        const results = await pool.query("SELECT TO_CHAR(date, 'Month') AS month_name, SUM(fuel_usage) AS total_fuel_usage FROM tbl_fuel_usage GROUP BY month_name ORDER BY MIN(date);");
         res.send(results.rows)
     } catch (err) {
         // console.error("error executing query:", err);
